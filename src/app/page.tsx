7937631703fa45cc3885a7ad2772e055dc68f742
D4ichi-Sugiy4m4 <ace.f6f.5.resipuro@gmail.com>
2023-11-24 01:57:49 +0900

    add blog page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import { AppBar, Box, CssBaseline, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Card,
+  CssBaseline,
+  Grid,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
 export default function Home() {
   return (
     <>
-      <CssBaseline/>
+      <CssBaseline />
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static">
           <Toolbar>
@@ -12,7 +21,21 @@ export default function Home() {
             </Typography>
           </Toolbar>
         </AppBar>
+        {/* ブログページ */}
+        <Grid container>
+          <Grid item xs={2} p={4}>
+              <Card>目次</Card>
+          </Grid>
+          <Grid item xs={7} p={4}>
+            <Stack spacing={4}>
+            <Card>コンテンツ</Card>
+            </Stack>
+          </Grid>
+          <Grid item xs={3} p={4}>
+            <Card>自己紹介などなど</Card>
+          </Grid>
+        </Grid>
       </Box>
     </>
-  )
+  );
 }
---
+++ src/app/utils/theme.ts
@@ -0,0 +1,9 @@
+import { cyan } from "@mui/material/colors"
+
+const color = {
+  background: {
+    main: cyan[50],
+  }
+}
+
+export const backGroundMain = color.background.main;
